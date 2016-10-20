7e992f9373f1051466f7e05e294f4e1b7fb25c58
Milo Hartsoe <milo.red.duck@gmail.com>
2016-10-20 16:01:57 +0000

    server

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var port= process.env.PORT || 3500;
+app.set('port',port);
+app.get("/",function(req,res){
+    var lang = req.headers["accept-language"];
+    var opsys = req.headers["user-agent"].match(/\(.+?\)/)[0];
+    lang = lang.substring(0,lang.indexOf(','));
+     res.json({
+        ipaddress: req.connection.remoteAddres || req.headers['x-forwarded-for'],
+        language: lang,
+        os: opsys
+    });
+})
+app.listen(port,function(){
+    console.log('listening on port ' + port);
+})
